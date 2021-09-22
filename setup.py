9babc5d797c2c9b2cee7e836fda10da2f9cae6f0
Eric Ulrich <eulrich@login.curnagl>
2021-09-22 11:50:14 +0200

    fixed typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='deltec',
       version='1.0',
-      description='Detects deletion and regions with no alignments in BAM file derived from haploid genomes.',
+      description='Detects deletions and regions with no alignments in BAM files derived from haploid genomes.',
       author='Eric Ulrich',
       url='https://github.com/nahanoo/deletion_detection',
       packages=['deletion_detection'],
