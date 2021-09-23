9493ce15d48fd5a9a33c5ef298e1e5c880c2cb98
Eric Ulrich <eulrich@login.curnagl>
2021-09-23 16:28:04 +0200

    MASSIVE speed improvements. Realized that pysam.count() is extremly slow.

--- deletion_detection/deletion_detection.py
+++ deletion_detection/deletion_detection.py
@@ -1,8 +1,20 @@
 import pysam
 import pandas as pd
 import os
+import subprocess
+from io import StringIO
 
 class Deletion():
+    def get_coverage(self,path):
+        cmd = ['samtools','depth','-a','-J','-Q','60',path]
+        process = subprocess.run(cmd,capture_output=True)
+        coverage = pd.read_csv(StringIO(process.stdout.decode()),sep='\t')
+        coverage.columns=['chromosome','position','coverage']
+        self.coverage = dict()
+        for chromosome,position,coverage in zip(coverage['chromosome'],\
+            coverage['position'],coverage['coverage']):
+            self.coverage[(chromosome,position)] = coverage
+
     def get_deletions(self,path):
         """This functions iterates over all primary reads in the alignment file.
         The CIGAR code of every read is checked for deletions.
@@ -14,7 +26,9 @@ class Deletion():
         for read in alignment:
             if not read.is_secondary:
                 reads.append(read)
-        for read in reads:
+        total_reads = len(reads)
+        self.get_coverage(path)
+        for counter,read in enumerate(reads):
             #We set the block position to zero which stands for the position in the reference.
             block_pos = 0
             #We set the cigar position to zero which stands for the position within the cigar code.
@@ -36,11 +50,12 @@ class Deletion():
                     #Deletion is stored in dictionary.
                     #Values are ([count,coverage],length)
                     #pysam is 0 index based, to make it compareable with samtools I switched to 1 indexing for storing.
-                    if (read.reference_name,read.pos+block_pos+1) in self.deletions.keys():
+                    if self.deletions.get((read.reference_name,read.pos+block_pos+1)) != None:
                         self.deletions[(read.reference_name,read.pos+block_pos+1)][0][0] += 1
+                        coverage = self.coverage[(read.reference_name,read.pos+block_pos+1)]
+                        self.deletions[(read.reference_name,read.pos+block_pos+1)][0][1] = coverage
                     else:
-                        coverage = alignment.count(read.reference_name,read.pos+block_pos,read.pos+block_pos+1)
-                        self.deletions[(read.reference_name,read.pos+block_pos+1)] = ([1,coverage],block_len)
+                        self.deletions[(read.reference_name,read.pos+block_pos+1)] = ([1,None],block_len)
                     block_pos += block_len
                 #3 is skipped region from the reference
                 elif block_type == 3:
@@ -68,3 +83,6 @@ class Deletion():
             df.at[counter,'coverage'] = count_coverage[1]
             df.at[counter,'type'] = 'in-read deletion'
         df.to_csv(os.path.join(out,'in_read_deletions.tsv'),index=False,sep='\t')
+
+d = Deletion()
+d.get_deletions('/users/eulrich/work/genome_size/data/01/alignment.reads.minimap.sorted.bam')
