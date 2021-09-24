d384bfc8b56a37f2c6e913e16a511a7fc8b72346
Eric Ulrich <eulrich@login.curnagl>
2021-09-24 11:10:25 +0200

    fixed typos

--- deletion_detection/main.py
+++ deletion_detection/main.py
@@ -7,12 +7,12 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Deletion detection based on sorted BAM files.\
         By default it only detects deletions which are located in reads.\
         Adding the --output_no_alignment_regions will also output the regions\
-        in the reference where no reads aligned. This feature requires SAMtools>=1.11 in your PATH.\
+        in the reference where no reads aligned. This package requires SAMtools>=1.11 in your PATH.\
         ')
     parser.add_argument('bam_file',help='path to sorted BAM file.')
     parser.add_argument('output_dir',help='output direcotry.')
     parser.add_argument('--output_no_alignment_regions',required=False,action='store_true',help='outputs the regions\
-        in the reference where no reads aligned. This feature requires SAMtools>=1.11 in your PATH.')
+        in the reference where no reads aligned.')
     parser.add_argument('--min_counts',type=int,help='minimal observed reads with the deletion [default is 5]')
     parser.add_argument('--min_frequency',type=float,help='minimal observed frequency of the deletion (observed reads with\
         the deletion divided by the coverage at this position) [default is 0.8]')
