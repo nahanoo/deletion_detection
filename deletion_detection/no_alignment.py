bbdd5debd5334b72fbbed9b983bba1f53852ff88
Eric Ulrich <eulrich@login.curnagl>
2021-09-24 11:07:03 +0200

    added mapping quality flag

--- deletion_detection/no_alignment.py
+++ deletion_detection/no_alignment.py
@@ -13,16 +13,18 @@ class NoAlignment():
     poor assembly quality which can lead to zero coverage. 
     """
 
-    def get_coverage(self,path):
+    def get_coverage(self,path,min_mapping_quality):
         """This function calculates coverage using samtools depth.
         Depending on the size of the bam file this can take a while
         and you may want to run it on a cluster. The flag "-J" is used
         which means that deletions in reads count as coverage.
         Therefore coverage is only zero in areas where no reads align.
         This function requires that samtools>=1.11 is in your PATH variable.
+        Depending on the sequencing data it's worth experimenting with the
+        --min_mapping_quality flag to get the desired results.
         """
         self.bamfile_path = path
-        cmd = ['samtools','depth','-a','-J','-Q','60',self.bamfile_path]
+        cmd = ['samtools','depth','-a','-J','-Q',str(min_mapping_quality),self.bamfile_path]
         process = subprocess.run(cmd,capture_output=True)
         self.coverage = pd.read_csv(StringIO(process.stdout.decode()),sep='\t')
         self.coverage.columns=['chromosome','position','coverage']
