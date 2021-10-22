73d26876ceb4b87f3e84767fc1de7a0947a3a8d5
nahanaoo <ericsven.u@gmail.com>
2021-10-22 16:16:12 +0200

    grabs all genes and product, nt and aa pos missing yet

--- deletion_detection/annotation.py
+++ deletion_detection/annotation.py
@@ -2,47 +2,61 @@ import pandas as pd
 from deletion_detection import Deletion
 from no_alignment import NoAlignment
 from Bio import SeqIO
+import numpy as np
 
 class Annotation():
-    def __init__(self,input):
-        self.input = input
-
-    def normalize_input(self):
-        raw = self.input
-        self.input = {key:None for key in raw.keys()}
-        for key,value in raw.items():
-            self.input[key] = value[1]
-
     def parse_genbank(self,genbank):
         contigs = [contig for contig in SeqIO.parse(genbank,'genbank')]
         return contigs
 
+def annotate(features,df):
+    out = pd.DataFrame(columns=df.columns.to_list()+\
+        ['coding_type','nt_pos','aa_pos','gene','product'])
+    loc = 0
+    for i,row in df.iterrows():
+        c,p,l = row['chromosome'],row['position'],row['length']
+        for (start,end),feature in features[c].items():
+            for nucleotide in range(start,end):
+                if nucleotide in range(p,p+l):
+                    try:
+                        gene = feature['gene']
+                    except KeyError:
+                        gene = np.nan
+                    row['position'] 
+                    entries = [feature['type'],np.nan,np.nan,gene,feature['product']]
+                    print(row.to_list()+entries)
+                    out.loc[loc] = row.to_list()+entries
+                    loc += 1
+                    break
+    return out
+
 def parse_genbank():
-    features = dict()
     contigs = [contig for contig in SeqIO.parse('testdata/at.gbk','genbank')]
+    features = {key:dict() for key in [contig.id for contig in contigs]}
     for contig in contigs:
         for f in contig.features:
             feature = dict()
-            feature['start'] = f.location.start
-            feature['end'] = f.location.end
-            feature['strand'] = f.location.strand
             feature['type'] = f.type
+            feature['strand'] = f.location.strand
             q = f.qualifiers
-            try:
-                feature['gene'] = q['gene']
-            except KeyError:
-                pass
-            try:
-                feature['inference'] = q['inference']
-            except KeyError:
-                pass
-            try:
-                feature['product'] = q['product']
-            except KeyError:
-                pass
-            features[(contig.id,f.location.start)] = feature
+            if not f.type == 'source':
+                try:
+                    feature['gene'] = q['gene'][0]
+                except KeyError:
+                    pass
+                try:
+                    feature['product'] = q['product'][0]
+                except KeyError:
+                    pass
+                features[contig.id][(int(f.location.start),int(f.location.end))] = feature
     return features
+
 f = parse_genbank()
+df = pd.concat([pd.read_csv('testdata/in_read_deletions.tsv',sep='\t'),
+    pd.read_csv('testdata/no_alignment_regions.tsv',sep='\t')])
+df.index = range(len(df))
+deletions = {(chromosome,position):length for chromosome,position,length in zip(df['chromosome'],df['position'],df['length'])}
+df = annotate(f,df)
     
 """
 d = Deletion()
