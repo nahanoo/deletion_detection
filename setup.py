6b0946d287be87bd53cf43c079337e5657c5db4c
nahanaoo <ericsven.u@gmail.com>
2022-03-09 18:43:51 +0100

    updated requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(name='deletion_detection',
       version='1.0',
-      description='Detects deletions and regions with no alignments in BAM files derived from haploid genomes.',
+      description='Detects deletions in BAM files derived from haploid genomes.',
       author='Eric Ulrich',
       url='https://github.com/nahanoo/deletion_detection',
       packages=['deletion_detection'],
       install_requires=['pandas',
-                        'pysam'],
+                        'pysam',
+                        'Bio',
+                        'dna_features_viewer'],
       entry_points={
           'console_scripts': [
               'detect_deletions = deletion_detection.main:main'
