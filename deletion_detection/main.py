83a5cbef98bb48576ccd01ee04da5102f1c27deb
nahanaoo <ericsven.u@gmail.com>
2022-03-09 18:43:29 +0100

    latest launcher

--- deletion_detection/main.py
+++ deletion_detection/main.py
@@ -5,28 +5,39 @@ from os.path import join
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Detect insertions in evolved bacterial strains. Fast and simpple.')
+        description='Detect insertions in evolved bacterial strains.')
     parser.add_argument(
-        'anceteral', help='fasta file of the ancesteral strain'
+        'ancestral', help='fasta file of the ancestral strain'
     )
     parser.add_argument(
         'mutant', help='genbank file of the mutated strain.')
     parser.add_argument('out_dir', help='output directory')
-    parser.add_argument('--plot', help='if this flag is added the alignment of every insertion\
-        is plotted.', action='store_true')
+    parser.add_argument('--plot', help='plots alignment of deletions', action='store_true')
 
     return parser.parse_args()
 
+def main():
+    args = parse_args()
+    # Class innit
+    d = Deletion(args)
+    # Chunk assembly into smaller sequences using a sliding window
+    d.chunk_assembly()
+    # Map chunked mutant assembly to reference
+    d.map_chunks()
+    # Gets all areas with no coverage
+    d.get_deletions()
+    # Gets deletions location in mutant
+    d.get_location()
+    # Annotates location
+    d.annotate()
+    # Dumps to file
+    d.dump()
+    if args.plot:
+        # Plot alignment of no coverage region in reference
+        d.plot_deletions()
+        # Plots annotations
+        d.plot_annotation()
+    # Cleans temporary files
+    d.clean()
 
-args = parse_args()
-d = Deletion(args)
-d.chunk_assembly()
-d.mapper(d.reference_fasta, join(d.out_dir, "chunked_sequences.fasta"),
-         join(d.out_dir, "aligned.sam"))
-d.get_deletions()
-d.get_mut_origin()
-d.dump()
-if args.plot:
-    d.plot_deletions()
-    d.plot_annotation()
-# i.clean()
+main()
